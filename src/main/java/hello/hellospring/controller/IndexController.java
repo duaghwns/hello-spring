da0b2ecbbf81547dc316d7efe3967574f114f0a4
duaghwns <hj2758@gmail.com>
2022-03-12 16:41:11 +0900

    model attribute

--- src/main/java/hello/hellospring/controller/IndexController.java
+++ src/main/java/hello/hellospring/controller/IndexController.java
@@ -1,13 +1,15 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("name","염호준");
         return "index";
     }
 }
