e2c865fad0475c4950c165fce105ff644b2a5c2b
duaghwns <hj2758@gmail.com>
2022-03-16 22:56:24 +0900

    DI

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,15 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+
+    private MemberService service;
+    @Autowired
+    public MemberController(MemberService service) {
+        this.service = service;
+    }
 }
--- src/main/java/hello/hellospring/service/impl/MemberServiceImpl.java
+++ src/main/java/hello/hellospring/service/impl/MemberServiceImpl.java
@@ -1,7 +1,5 @@
 package hello.hellospring.service.impl;
 
-import org.springframework.stereotype.Service;
 
-@Service
 public class MemberServiceImpl {
 }
