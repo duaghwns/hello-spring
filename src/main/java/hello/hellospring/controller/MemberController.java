b3996cf4dc29e0ae6c248dfaa7c8b4d04b0bb560
duaghwns <hj2758@gmail.com>
2022-03-17 00:16:46 +0900

    Spring Bean

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -2,9 +2,7 @@ package hello.hellospring.controller;
 
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
-@Controller
 public class MemberController {
 
     private MemberService service;
